2d84d8a4cee173e3904a8775957d7b35635e95bb
LiwaDladloti <liwadladloti@gmail.com>
2017-03-03 15:40:19 +0200

    added functions in greet.js for user to select preferd language for them to be greeted by selecting the radio buttons I added

--- greet.js
+++ greet.js
@@ -1,5 +1,17 @@
-function greetMe() {
+/*function greetMe() {
     'use strict';
     var x = document.getElementById('TextBox').value;
     document.getElementById('output').innerHTML = 'Hello, ' + x;
+}*/
+
+function myName() {
+    'use strict';
+    var xxx = document.getElementById('TextBox').value;
+    if (document.getElementById('English').checked === true && xxx.length > 0) {
+        document.getElementById('output').innerHTML = 'Hello, ' + xxx;
+    } else if (document.getElementById('isiXhosa').checked === true && xxx.length > 0) {
+        document.getElementById('output').innerHTML = 'Molo, ' + xxx;
+    } else if (document.getElementById('Latin').checked === true && xxx.length > 0) {
+        document.getElementById('output').innerHTML = 'Salve, ' + xxx;
+    }
 }
