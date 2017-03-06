cf563ca0497c70892ee0aaab55a1e1a59f054555
LiwaDladloti <liwadladloti@gmail.com>
2017-03-06 10:13:47 +0200

    added a function that counts how many times a person has greeted

--- greet.js
+++ greet.js
@@ -4,14 +4,20 @@
     document.getElementById('output').innerHTML = 'Hello, ' + x;
 }*/
 
+var clickCount = 0;
 function myName() {
     'use strict';
     var xxx = document.getElementById('TextBox').value;
     if (document.getElementById('English').checked === true && xxx.length > 0) {
         document.getElementById('output').innerHTML = 'Hello, ' + xxx;
+        clickCount += 1;
     } else if (document.getElementById('isiXhosa').checked === true && xxx.length > 0) {
         document.getElementById('output').innerHTML = 'Molo, ' + xxx;
+        clickCount += 1;
     } else if (document.getElementById('Latin').checked === true && xxx.length > 0) {
         document.getElementById('output').innerHTML = 'Salve, ' + xxx;
+        clickCount += 1;
     }
+    document.getElementById('TextBox').value = "";
+    document.getElementById('greetCount').innerHTML = 'Greets: ' + clickCount;
 }
